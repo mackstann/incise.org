a67bc8d5d35ec4cf3d5846909d715761257a8d6e
Nick Welch <mack@incise.org>
2008-06-04 03:58:49 -0700

    blah

--- header.sh
+++ header.sh
@@ -1,5 +1,22 @@
-cat header.html
-echo '<h1><a href="/">[incise.org]</a> '
-echo "$1" | sed -e 's/-/ /g'
-echo "</h1>"
+if [ "$1" = "py-libmpdclient2" ]
+then
+    spacey_name="$1"
+else
+    spacey_name=`echo "$1" | sed -e 's/-/ /g'`
+fi
+
+echo "
+<html>
+<head>
+<title>incise.org - $1</title>
+</title>
+<style type="text/css">
+    table, tr { background: #aaa; }
+    td, th { background: white; }
+    pre { background: #ffe; border: 1px solid #666; padding: 5px; }
+</style>
+</head>
+<body>
+<h1><a href="/">[incise.org]</a> $spacey_name</h1>
+"
 
