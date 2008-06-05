17d6db7d5796448905c7ad5952b7e5be4cf17e93
Nick Welch <mack@incise.org>
2008-06-05 01:18:39 -0700

    screw mtimes

--- footer.sh
+++ footer.sh
@@ -1,11 +1,7 @@
-pagemod=`ls -l pages/"$1".html | awk '{print $6, $7}'`
-sitemod=`ls -ld pages | awk '{print $6, $7}'`
 echo "
 <hr>
 <address>
-Nick Welch <a href=\"mailto:nick@incise.org\">&lt;nick@incise.org&gt;</a><br>
-This page last modified on $pagemod.<br>
-incise.org last modified on $sitemod.
+Nick Welch <a href=\"mailto:nick@incise.org\">&lt;nick@incise.org&gt;</a>
 </address>
 </body>
 </html>
