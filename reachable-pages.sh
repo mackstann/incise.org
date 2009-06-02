bb5e8d6eec5812f38c893090170451f5de2c4002
Nick Welch <mack@incise.org>
2009-06-01 20:29:48 -0700

    deleting old build system

--- reachable-pages.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-cd /tmp
-wget -r -nd --delete-after -A .html http://incise.org 2>&1 |
-    grep ^-- |
-    cut -d' ' -f3 |
-    grep '^http://incise.org/.*\.html$' |
-    sed 's,^http://incise.org/,,' |
-    sort -u
-
